b6ac0f7cea51e15ea4d10a9460beadc88eace05b
TejaswiChava <teja.chava4@gmail.com>
2018-06-25 10:59:23 +0530

    initial commit

---
+++ src/constants/api.js
@@ -0,0 +1,14 @@
+import Config from 'react-native-config';
+
+export const TMDB_URL = "https://api.themoviedb.org/3";
+export const TMDB_IMG_URL = "https://image.tmdb.org/t/p";
+export const TMDB_API_KEY = "3e5c255c8fa5faf3ebdb249e2efa594c";
+
+export const YOUTUBE_URL = "https://www.googleapis.com/youtube/v3/videos";
+export const YOUTUBE_API_KEY = "AIzaSyD6nCTWXcwZgLlZvzyMR2C9ZUsgn5GjUgY";
+// TMDB_URL=https://api.themoviedb.org/3
+// TMDB_IMG_URL=https://image.tmdb.org/t/p
+// TMDB_API_KEY=your_tmdb_api_key_here
+
+// YOUTUBE_URL=https://www.googleapis.com/youtube/v3/videos
+// YOUTUBE_API_KEY=your_youtube_api_key_here-
